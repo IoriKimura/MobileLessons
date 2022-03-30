32fe09a13bb211f73c834a57f9bde75d0e93fa76
Serge <sergeykonstwork@gmail.com>
2022-03-30 12:31:04 +0300

    Завершённое задание ResultActivity

---
+++ practice3/resultactivity/src/main/java/ru/mirea/ivanov/resultactivity/DataActivity.java
@@ -0,0 +1,27 @@
+package ru.mirea.ivanov.resultactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class DataActivity extends AppCompatActivity {
+
+    private EditText universityText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+        universityText = findViewById(R.id.universityEditText);
+    }
+
+    public void sendResult(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("name", universityText.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
