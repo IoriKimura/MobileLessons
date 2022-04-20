f4d25ae3ef703a735dc62b8460b2729baddb25bf
Serge <sergeykonstwork@gmail.com>
2022-04-20 19:52:12 +0300

    Контрольное задание + маленькие фиксы

--- mirea project/app/src/main/java/ru/mirea/ivanov/mireaproject/MainActivity.java
+++ mirea project/app/src/main/java/ru/mirea/ivanov/mireaproject/MainActivity.java
@@ -2,18 +2,18 @@ package ru.mirea.ivanov.mireaproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import ru.mirea.ivanov.mireaproject.databinding.ActivityMainBinding;
 import ru.mirea.ivanov.mireaproject.ui.mediaPlayer.PlayerService;
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.webResFragment)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
